cd80c13703bc1058205527da752943f5a8e62ec3
Irene Strigo <irene.strigo@gmail.com>
2024-04-14 20:31:13 +0200

    feat: one more task

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -207,8 +207,10 @@ function logger(/* func, logFunc */) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  return function funk(...args) {
+    return fn.apply(this, args1.concat(args));
+  };
 }
 
 /**
