afa4cf5381007788cc4ea2e59ef31dd6449e7c47
Irene Strigo <irene.strigo@gmail.com>
2024-04-14 20:09:54 +0200

    feat: one more task

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -153,8 +153,18 @@ function memoize(func) {
  * }, 2);
  * retryer() => 2
  */
-function retry(/* func, attempts */) {
-  throw new Error('Not implemented');
+function retry(func, attempts) {
+  return function fn() {
+    let error = new Error('test');
+    for (let i = 0; i < attempts; i += 1) {
+      try {
+        return func();
+      } catch (err) {
+        error = err;
+      }
+    }
+    throw error;
+  };
 }
 
 /**
