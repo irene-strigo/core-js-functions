f05c338c7507b1fe4d5098d8945c3fa641bf3f8f
Irene Strigo <irene.strigo@gmail.com>
2024-04-15 13:36:39 +0200

    feat: one more task

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -230,10 +230,17 @@ function partialUsingArguments(fn, ...args1) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  function* idGenerator() {
+    for (let i = 0; i < 1000; i += 1) {
+      yield startFrom + i;
+    }
+  }
+  const newId = idGenerator();
+  return function foo() {
+    return newId.next().value;
+  };
 }
-
 module.exports = {
   getCurrentFunctionName,
   getFunctionBody,
